0dc7e21d0908f50d895521e1826c87482a2285a0
BoyellaRamadevi <41728410+BoyellaRamadevi@users.noreply.github.com>
2018-07-27 11:14:57 +0530

    Update Bhanu.java

--- Bhanu.java
+++ Bhanu.java
@@ -11,7 +11,7 @@ class Bhanu
 	{
 	 Scanner reader = new Scanner(System.in);
 
-        System.out.print("Enter a number: ");
+        System.out.println("Enter a number: ");
         int num = reader.nextInt();
 
         if(num % 2 == 0)

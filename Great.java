0927cd3844394eeea79dafea2af80ada1fe69e23
BoyellaRamadevi <41728410+BoyellaRamadevi@users.noreply.github.com>
2018-07-27 13:20:45 +0530

    Create Great.java

---
+++ Great.java
@@ -0,0 +1,31 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		if(a>b && a>c)
+		   {
+		   	System.out.println(a);
+		   }
+		 else if(b>c)
+		   {
+		 	  System.out.println(b);
+		   }
+		   else
+	       {
+		   	System.out.println(c);
+		   }		
+
+	}
+}
+ 

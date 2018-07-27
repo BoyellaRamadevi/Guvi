f03a9f217ea4733cbeca2afea241c9184f7d930e
BoyellaRamadevi <41728410+BoyellaRamadevi@users.noreply.github.com>
2018-07-27 13:06:35 +0530

    Create Largest.java

---
+++ Largest.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Largest
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+
+        int n1 = -4, n2 = 3, n3 = 5;
+
+        if( n1 >= n2 && n1 >= n3)
+            System.out.println(n1 );
+
+        else if (n2 >= n1 && n2 >= n3)
+            System.out.println(n2);
+
+        else
+            System.out.println(n3);
+    }
+}
+	
